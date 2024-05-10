9d104d5dfd8d3a047c3d377af8d511f019e338c4
rifat-sorkar <rifatpot72@gmail.com>
2024-05-10 11:13:55 +0600

    script.js file is uploded

---
+++ script.js
@@ -0,0 +1,57 @@
+// Data Types are 2 types
+
+// Premetive 
+
+//     String
+const str = "Rifat"
+
+console.log(typeof str)
+//     Number
+const num = 23.3
+
+console.log(typeof num)
+//     Boolean
+ 
+const bool = true
+
+console.log(typeof bool)
+//     undifined
+function x(arr){
+  
+}
+console.log(x())
+// null
+
+const n = null
+
+console.log(typeof n)
+
+
+
+//     Symbol
+
+const sym = Symbol()
+
+console.log(typeof sym)
+
+//  Non-Premetive
+
+//  Object
+
+//     Object
+const obj = {
+    name:'rifat',
+    age:23
+}
+const obj2 = new Object()
+
+console.log(obj,obj2)
+
+//     Array
+ const arr = ['rifat',12122,null,true]
+
+ console.log(typeof arr)
+//     Date
+  const date = Date()
+
+  console.log(date)
