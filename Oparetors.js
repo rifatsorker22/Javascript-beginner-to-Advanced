db995b1846e5467afc1dcd6c906a03261be66dfc
rifat-sorkar <rifatpot72@gmail.com>
2024-05-10 12:17:04 +0600

    Oparetor is added

---
+++ Oparetors.js
@@ -0,0 +1,33 @@
+// Operators
+
+// Arithmetic Operators
+
+let num1 = 10
+
+let num2 = 5 * 5 * 5
+
+let sum = num1 + num2
+
+let sub = num1 - num2
+
+let multiplication = num1 * num2
+
+let division = num1 / num2
+
+let expo = num1 ** num1
+
+let modu = 100 / 10
+
+let increment = ++num1
+
+let decrement = --num1
+
+
+console.log(sum)
+console.log(sub)
+console.log(multiplication)
+console.log(division)
+console.log(expo)
+console.log(modu)
+console.log(increment)
+console.log(decrement)
