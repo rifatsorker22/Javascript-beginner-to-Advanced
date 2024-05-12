0f36e2beeb6897587760bce46f4023b1ce924f4d
rifat-sorkar <rifatpot72@gmail.com>
2024-05-12 19:14:46 +0600

    update oparetors

--- Oparetors.js
+++ Oparetors.js
@@ -1,3 +1,40 @@
+// Operators
+
+// Arithmetic Operators
+
+let num1 = 10
+
+let num2 = 5 * 5 * 5
+
+let sum = num1 + num2
+
+let sub = num1 - num2
+
+let multiplication = num1 * num2
+
+let division = num1 / num2
+
+let expo = num1 ** num1
+
+let modu = 100 / 10
+
+let increment = ++num1
+
+let decrement = --num1
+
+
+console.log(sum)
+console.log(sub)
+console.log(multiplication)
+console.log(division)
+console.log(expo)
+console.log(modu)
+console.log(increment)
+console.log(decrement)
+
+
+// logical Oparetors
+
 let number = 22
 
 let number2 = 23
@@ -9,6 +46,5 @@ console.log(number !== number2)  // !==
 console.log(number > number2)
 
 console.log(number <= number2)
-
 
 console.log(number >= number2)
